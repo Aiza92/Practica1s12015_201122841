b9aab10a6e7d1603d1d13a61dd0db1653f50315d
Any <Any@AIZA>
2015-02-13 15:32:23 -0600

    Primera actualizacion

---
+++ Practica1edd_201122841/src/practica1edd_201122841/Practica1edd_201122841.java
@@ -0,0 +1,21 @@
+package practica1edd_201122841;
+
+
+
+
+/**
+ *
+ * @author Any
+ */
+public class Practica1edd_201122841 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MENU principal = new MENU (); 
+       principal.show();
+    
+    }
+    
+}
